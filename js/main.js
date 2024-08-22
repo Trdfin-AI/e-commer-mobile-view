b6683679b5c610ca99643458965128d7ee93a84b
murali01K <murali.k@trdfin.in>
2024-08-22 11:42:41 +0530

    updates 20

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
+// preloader 
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 3000); // 3 seconds
+});
+//pre loader-end
+
+// order accept code home page start//
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.accept').forEach(button => {
+        button.addEventListener('click', function () {
+            const orderId = this.getAttribute('data-order-id');
+            // Redirect to the order details page with the order ID
+            window.location.href = `orders-detail.html?orderId=${orderId}`;
+        });
+    });
+});
+
+// order accept code home page end //
+
 // date and time js code
 function getDateTime() {
-    var now = new Date(); 
+    var now = new Date();
     var year = now.getFullYear();
-    var month = now.getMonth() + 1; 
+    var month = now.getMonth() + 1;
     var day = now.getDate();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
-    
+
     // Convert 24-hour format to 12-hour format
     var period = hour >= 12 ? 'PM' : 'AM';
     hour = hour % 12;
-    hour = hour ? hour : 12; 
-    
+    hour = hour ? hour : 12;
+
     month = month < 10 ? '0' + month : month;
     day = day < 10 ? '0' + day : day;
     minute = minute < 10 ? '0' + minute : minute;
     second = second < 10 ? '0' + second : second;
-    
+
     var dateTime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ' ' + period;
-    
+
     return dateTime;
 }
 
-setInterval(function(){
+setInterval(function () {
     var currentTime = getDateTime();
     document.getElementById("current-dt").innerHTML = currentTime;
 }, 1000);
@@ -72,3 +95,5 @@ document.addEventListener('DOMContentLoaded', function () {
         profileLocation.textContent = document.getElementById('address').value;
     });
 });
+// profile js code end //
+
